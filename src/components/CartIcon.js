54ec58ce8cd601cfec001ffbc81d9b23673016c0
Zamur Vedovatto <vedovatto@gmail.com>
2021-04-28 00:46:16 -0300

    items num

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux'
 
 export default function CartIcon() {
+	const badgeNumber = useSelector((state) => state.cart.items.length)
+	console.log("carrinho numero", badgeNumber)
 	// let itemsCount = this.props.cart.reduce((acc, item) => acc + item.qty, 0);
-	const [itemsCount, setItemsCount] = useState(1) // TODO calcular
 
 	return (
 		<Link to="/cart">
 			<div className="cartIcon">
 				<i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
-				{itemsCount > 0 &&
+				{badgeNumber > 0 &&
 					<div className="cartBadge">
-						{itemsCount}
+						{badgeNumber}
 					</div>
 				}
 			</div>
